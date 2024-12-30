c870f88ccab02844d63245ec026d08dd1bb10ae4
Aya Igarashi <ladiclexxx@gmail.com>
2024-12-30 23:08:33 +0900

    fix: remove pflag

--- main.go
+++ main.go
@@ -3,15 +3,8 @@ package main
 import (
 	"log"
 
-	"github.com/spf13/pflag"
-
 	"github.com/Ladicle/tcardgen/cmd"
 )
-
-func init() {
-	flags := pflag.NewFlagSet("tcardgen", pflag.ExitOnError)
-	pflag.CommandLine = flags
-}
 
 func main() {
 	if err := cmd.NewRootCmd().Execute(); err != nil {
