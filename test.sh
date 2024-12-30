36718bc331f2521595b2d5fb3272572b2e3b8e57
Aya Igarashi <ladiclexxx@gmail.com>
2024-12-30 23:21:58 +0900

    ci: add test script

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -xeo pipefail
+
+FNAME=KintoSans.zip
+VERSION=${VERSION:-v1.0.1}
+
+# Download Fonts to `font` directory
+if [ ! -f font/KintoSans-Regular.ttf ]; then
+    mkdir -p font
+    wget https://github.com/ookamiinc/kinto/releases/download/${VERSION}/${FNAME}
+    unzip -j $FNAME "*.ttf" -d font
+    rm $FNAME
+fi
+
+# Generate an image and check diff
+[ -d test ] && rm -r test
+mkdir -p test
+for name in "blog-post" "blog-post2"; do
+    echo "Test $name"
+    go run main.go \
+       -f font \
+       -o test/ \
+       -t example/template.png \
+       example/$name.md
+    diff test/$name.png example/$name.png
+done
